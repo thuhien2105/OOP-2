b37bd7c46b221ea322736f71d8d6fff4dc638cf0
hoaian2206 <95273668+hoaian2206@users.noreply.github.com>
2021-11-30 14:13:20 +0700

    Add files via upload

---
+++ CPEOPLE.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "Console.h"
+#include "CVEHICLE.h"
+
+class CPEOPLE {
+	string mPeople[2];
+	int mX, mY;
+	bool mState; //Trạng thái sống chết
+
+	void mDelete(); // delete last postion of people before move
+
+public:
+	CPEOPLE(int x = colBoundary / 2, int y = rowBoundary - 1, int state = true);
+	~CPEOPLE();
+	void Up();
+	void Left();
+	void Right();
+	void Down();
+	bool IsImpact();
+	bool IsFinish();
+	bool IsDead();
+
+	void DrawPeople();
+	void animationDie();
+	void saveGame(ofstream& fo);
+};
