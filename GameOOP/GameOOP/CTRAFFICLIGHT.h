b69296e2b8bdd53bafd1d3c46b99896ad1eb6d16
hoaian2206 <nhan20@clc.fitus.edu.vn>
2021-12-10 21:25:47 +0700

    add CTRAFFICLIGHT & Definition

---
+++ GameOOP/GameOOP/CTRAFFICLIGHT.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Definition.h"
+#include <fstream>
+
+
+class CTRAFFICLIGHT
+{
+private:
+	int m_state;
+	int m_count;
+public:
+	CTRAFFICLIGHT(int count = 5, int state = GREEN) :m_count(count), m_state(state) {}
+	~CTRAFFICLIGHT();
+
+	void printTrafficLight(int x, int y);
+	void switchState(int x, int y);
+	int getState();
+	void saveGame(std::ofstream& fo);
+};
+
---
+++ GameOOP/GameOOP/Definition.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+
+#define RED 0
+#define YELLOW 1
+#define GREEN 2
+
+#define COUNTDOWNCHANGELIGHT 10
+#define COUNTDOWN 18
